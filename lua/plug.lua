ad54ffe7e10717be4d51bb0c7422479823d6ab35
Skye Purchase <skyepurchase@gmail.com>
2024-09-29 20:55:12 +0100

    Add bootstrap code for vim-plug

---
+++ lua/plug.lua
@@ -0,0 +1,10 @@
+-- plug.lua
+
+-- bootstrap plug
+local data_dir = vim.fn.stdpath('data')
+if vim.fn.empty(vim.fn.glob(data_dir .. '/site/autoload/plug.vim')) == 1 then
+    vim.cmd('silent !curl -fLo ' .. data_dir .. '/site/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim')
+    vim.o.runtimepath = vim.o.runtimepath
+    vim.cmd('autocmd VimEnter * PlugInstall --sync | source ~/.config/nvim/old.vim')
+end
+
