26e5d2d359c4a8bbafdcba398fd9387753ec9dc0
Skye Purchase <skyepurchase@gmail.com>
2025-06-02 00:15:39 +0100

    Implement QOL lazy.nvim setup

--- lua/completion.lua
+++ lua/completion.lua
@@ -8,7 +8,7 @@ cmp.setup({
     snippet = {
         -- REQUIRED - you must specify a snippet engine
         expand = function(args)
-            vim.fn["UltiSnip#Anon"](args.body) -- `ultisnips`
+            vim.fn["UltiSnips#Anon"](args.body) -- `ultisnips`
         end,
     },
     mapping = {
--- lua/plugins/movement-spec.lua
+++ lua/plugins/movement-spec.lua
@@ -3,7 +3,6 @@
 -- Lazy.nvim movement plugins
 
 return {
-    { 'nvim-lua/plenary.nvim', version = false, priority = 1000 },
     {
         'nvim-telescope/telescope.nvim',
         cmd = "Telescope",
