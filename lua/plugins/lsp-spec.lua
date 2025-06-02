d45a7ce92d798b6906b197282d4892952b4576e4
Skye Purchase <skyepurchase@gmail.com>
2025-06-02 15:28:29 +0100

    Use a cleaner LSP configuration

--- lua/plugins/lsp-spec.lua
+++ lua/plugins/lsp-spec.lua
@@ -8,18 +8,18 @@ return {
         event = { "BufReadPre", "BufNewFile" },
         dependencies = {
             "hrsh7th/cmp-nvim-lsp",
-            -- TODO: lazydev if I do this more frequently
+            {
+                "mason-org/mason-lspconfig.nvim",
+                dependencies = { "mason-org/mason.nvim" }
+            }
         },
         config = function()
-            local nvim_lsp = require("lspconfig")
-            local mason_lspconfig = require("mason-lspconfig")
-
-            local protocol = require("vim.lsp.protocol")
+            local lspconfig = require("lspconfig")
 
             local on_attach = function(client, bufnr)
                 if client.server_capabilities.documentFormattingProvider then
                     vim.api.nvim_create_autocmd(
-                        "BufWritePre", 
+                        "BufWritePre",
                         {
                             group = vim.api.nvim_create_augroup(
                                 "Format", { clear = true }
@@ -39,84 +39,57 @@ return {
                 vim.lsp.protocol.make_client_capabilities()
             )
 
-            mason_lspconfig.setup({
-                function(server)
-                    nvim_lsp[server].setup({
-                        capabilities = capabilities,
-                    })
-                end,
-                ["ts_ls"] = function()
-                    nvim_lsp["ts_ls"].setup({
-                        on_attach = on_attach,
-                        capabilities = capabilities,
-                    })
-                end,
-                ["cssls"] = function()
-                    nvim_lsp["cssls"].setup({
-                        on_attach = on_attach,
-                        capabilities = capabilities,
-                    })
-                end,
-                ["html"] = function()
-                    nvim_lsp["html"].setup({
-                        on_attach = on_attach,
-                        capabilities = capabilities,
-                    })
-                end,
-                ["jsonls"] = function()
-                    nvim_lsp["jsonls"].setup({
-                        on_attach = on_attach,
-                        capabilities = capabilities,
-                    })
-                end,
-                ["eslint"] = function()
-                    nvim_lsp["eslint"].setup({
-                        on_attach = on_attach,
-                        capabilities = capabilities,
-                    })
-                end,
-                ["pyright"] = function()
-                    nvim_lsp["pyright"].setup({
-                        on_attach = on_attach,
-                        capabilities = capabilities,
-                    })
-                end,
-                ["marksman"] = function()
-                    nvim_lsp["marksman"].setup({
-                        on_attach = on_attach,
-                        capabilities = {
-                            workspace = {
-                                didChangeWatchedFiles = {
-                                    dynamicRegistration = true,
-                                },
-                            }
-                        },
-                    })
-                end,
+            lspconfig.ts_ls.setup({
+                on_attach = on_attach,
+                capabilities = capabilities,
             })
-        end
-    },
-    {
-        "mason-org/mason.nvim",
-        dependencies = {
-            "mason-org/mason-lspconfig.nvim",
-            -- TODO: look into mason-tool-installer
-        },
-        config = function()
-            require("mason").setup()
-
-            require("mason-lspconfig").setup({
-                automatic_installation = true,
-                ensure_installed = {
-                    "pyright",
-                    "eslint",
-                    "html",
-                    "cssls",
-                    "jsonls",
-                    "ts_ls",
-                    "marksman"
+            lspconfig.cssls.setup({
+                on_attach = on_attach,
+                capabilities = capabilities,
+            })
+            lspconfig.html.setup({
+                on_attach = on_attach,
+                capabilities = capabilities,
+            })
+            lspconfig.jsonls.setup({
+                on_attach = on_attach,
+                capabilities = capabilities,
+            })
+            lspconfig.eslint.setup({
+                on_attach = on_attach,
+                capabilities = capabilities,
+            })
+            lspconfig.pyright.setup({
+                on_attach = on_attach,
+                capabilities = capabilities,
+            })
+            lspconfig.marksman.setup({
+                on_attach = on_attach,
+                capabilities = {
+                    workspace = {
+                        didChangeWatchedFiles = {
+                            dynamicRegistration = true,
+                        },
+                    }
+                },
+            })
+            lspconfig.lua_ls.setup({
+                on_attach = on_attach,
+                capabilities = capabilities,
+                settings = {
+                    Lua = {
+                        diagnostics = {
+                            globals = { 'vim' },
+                        },
+                    },
                 },
             })
         end,
+    },
+    {
+        "mason-org/mason.nvim",
+        opts = {
+            PATH = 'append',
+        }
     }
 }
