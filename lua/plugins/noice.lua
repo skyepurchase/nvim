7f255226e22d93ea2b03bc163c232682afdb5179
Skye Purchase <skyepurchase@gmail.com>
2025-06-04 15:04:07 +0100

    Add git commit keybinding

--- lua/plugins/noice.lua
+++ lua/plugins/noice.lua
@@ -1,24 +1,24 @@
 -- lua/plugins/noice.lua
 
 return {
-	"folke/noice.nvim",
-	event = "VeryLazy",
-	dependencies = {
-		"MunifTanjim/nui.nvim",
-		"rcarriga/nvim-notify"
-	},
-	opts = {
-		lsp = {
-			override = {
-				["vim.lsp.util.convert_input_to_markdown_lines"] = true,
-				["vim.lsp.util.stylize_markdown"] = true,
-				["cmp.entry.get_documentation"] = true,
-			}
-		},
-		presets = {
-			long_message_to_split = true,
-			inc_rename = false,
-			lsp_doc_border = false,
-		}
-	}
+    "folke/noice.nvim",
+    event = "VeryLazy",
+    dependencies = {
+        "MunifTanjim/nui.nvim",
+        "rcarriga/nvim-notify"
+    },
+    opts = {
+        lsp = {
+            override = {
+                ["vim.lsp.util.convert_input_to_markdown_lines"] = true,
+                ["vim.lsp.util.stylize_markdown"] = true,
+                ["cmp.entry.get_documentation"] = true,
+            }
+        },
+        presets = {
+            long_message_to_split = true,
+            inc_rename = false,
+            lsp_doc_border = false,
+        }
+    }
 }
