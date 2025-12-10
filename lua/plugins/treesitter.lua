f517fffde89af272f0e18d7ece4ff52520e8334c
Skye Purchase <s.purchase25@imperial.ac.uk>
2025-12-10 14:55:29 +0000

    Add treesitter without highlighting
    It appears the treesitter highlighting breaks markdown $$ ultisnip
    completion. This does not occur in tex files.

---
+++ lua/plugins/treesitter.lua
@@ -0,0 +1,55 @@
+-- lua/plugins/treesitter.lua
+
+return {
+    "nvim-treesitter/nvim-treesitter",
+    event = { "BufReadPre", "BufNewFile" },
+    lazy = false,
+    branch = 'main',
+    build = ":TSUpdate",
+    dependencies = {
+        "nvim-treesitter/nvim-treesitter-textobjects",
+        "nvim-treesitter/nvim-treesitter-context",
+    },
+    config = function()
+        require("nvim-treesitter.configs").setup({
+            ensure_installed = {
+                "bash",
+                "css",
+                "html",
+                "javascript",
+                "json",
+                "lua",
+                "python",
+                "typescript",
+            },
+            -- highlight = {
+            --     enable = true,
+            -- },
+            indent = {
+                enable = true,
+            },
+            textobjects = {
+                select = {
+                    enable = true,
+                    lookahead = true,
+                    keymaps = {
+                        ["af"] = "@function.outer",
+                        ["if"] = "@function.inner",
+                        ["ac"] = "@class.outer",
+                        ["ic"] = "@class.inner",
+                    },
+                },
+            },
+        })
+
+        local tsc = require("treesitter-context")
+        tsc.setup({
+            enable = true,
+            max_lines = 0,
+            trim_scope = "outer",
+            min_window_height = 0,
+            mode = "topline",
+            zindex = 20,
+        })
+    end,
+}
